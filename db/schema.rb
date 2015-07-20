96832329c782c1552001062c67c5d34bcae9b2ac
LauraKirby <laurakirby26@gmail.com>
2015-07-19 19:10:15 -0700

    add superhero model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150719064517) do
+ActiveRecord::Schema.define(version: 20150720020932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "superheros", force: :cascade do |t|
+    t.string   "name"
+    t.string   "height"
+    t.text     "bio"
+    t.string   "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "teams", force: :cascade do |t|
     t.string   "name"
